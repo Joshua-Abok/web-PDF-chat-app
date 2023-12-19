c753ee05cda582dade79c0c0a38c85b2860b3cb9
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-12-19 22:13:33 +0300

    chains don't stream

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,13 +11,20 @@ prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
 ])
 
-messages = prompt.format_messages(content="tell me a joke")
+# wrap our lang model inside a chain 
+chain = LLMChain(llm=chat, prompt=prompt)
 
-# print(messages)
-# output = chat.invoke(messages)
-# output = chat.__call__(messages)
-output = chat.stream(messages)  # -> returns generator object BaseChatModel.stream
+for output in chain.stream(input={"content": "tell me a joke"}):
+    print(output)
 
-# take the output bit by bit 
-for message in chat.stream(messages):
-    print(message.content)
+
+# messages = prompt.format_messages(content="tell me a joke")
+
+# # print(messages)
+# # output = chat.invoke(messages)
+# # output = chat.__call__(messages)
+# output = chat.stream(messages)  # -> returns generator object BaseChatModel.stream
+
+# # take the output bit by bit 
+# for message in chat.stream(messages):
+#     print(message.content)

2d8f59c6a78c958d1e6e650e17adf78326a930ca
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-12-19 20:28:14 +0300

    build_chain & test

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,6 +1,10 @@
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 
+from langchain.chains import ConversationalRetrievalChain
+from app.chat.llms.chatopenai import build_llm
+from app.chat.memories.sql_memory import build_memory
+
 
 def build_chat(chat_args: ChatArgs):
     """
@@ -16,3 +20,14 @@ def build_chat(chat_args: ChatArgs):
     
     # build retriever -> nicely scoped 
     retriever = build_retriever(chat_args)
+
+    # build chain & return it 
+    llm = build_llm(chat_args)
+    memory = build_memory(chat_args)
+
+    return ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        memory=memory, 
+        retriever=retriever
+    )
+
